ad6c9399772b2eac79bd87ed099a8b0596a1774e
gitlab-runner <do-not-reply@cypress.com>
2019-10-18 09:06:16 -0500

    Upload mtb-example-psoc6-uart-transmit-receive 1.0.0.27

---
+++ main.c
@@ -0,0 +1,121 @@
+/******************************************************************************
+* File Name: main.c
+*
+* Description: This example demonstrates the UART transmit and receive
+*              operation using HAL APIs
+*
+* Related Document: See Readme.md 
+*
+*******************************************************************************
+* Copyright (2018-2019), Cypress Semiconductor Corporation. All rights reserved.
+*******************************************************************************
+* This software, including source code, documentation and related materials
+* (“Software”), is owned by Cypress Semiconductor Corporation or one of its
+* subsidiaries (“Cypress”) and is protected by and subject to worldwide patent
+* protection (United States and foreign), United States copyright laws and
+* international treaty provisions. Therefore, you may use this Software only
+* as provided in the license agreement accompanying the software package from
+* which you obtained this Software (“EULA”).
+*
+* If no EULA applies, Cypress hereby grants you a personal, nonexclusive,
+* non-transferable license to copy, modify, and compile the Software source
+* code solely for use in connection with Cypress’s integrated circuit products.
+* Any reproduction, modification, translation, compilation, or representation
+* of this Software except as specified above is prohibited without the express
+* written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death (“High Risk Product”). By
+* including Cypress’s product in a High Risk Product, the manufacturer of such
+* system or application assumes all risk of such use and in doing so agrees to
+* indemnify Cypress against all liability.
+*******************************************************************************/
+
+#include "cy_pdl.h"
+#include "cyhal.h"
+#include "cybsp.h"
+#include "cy_retarget_io.h"
+
+/*******************************************************************************
+* Function Name: handle_error
+********************************************************************************
+* Summary:
+* User defined error handling function
+*
+* Parameters:
+*  void
+*
+* Return:
+*  void
+*
+*******************************************************************************/
+void handle_error(void)
+{
+     /* Disable all interrupts. */
+    __disable_irq();
+
+    CY_ASSERT(0);
+}
+
+/*******************************************************************************
+* Function Name: main
+********************************************************************************
+* Summary:
+* This is the main function for CM4 CPU.
+*	Reads one byte from the serial terminal and echoes back the read byte
+*
+* Parameters:
+*  void
+*
+* Return:
+*  int
+*
+*******************************************************************************/
+int main(void)
+{
+    cy_rslt_t result;
+    /* Set up the device based on configurator selections */
+    result = cybsp_init();
+	if(result != CY_RSLT_SUCCESS)
+	{
+		handle_error();
+	}
+    result = cy_retarget_io_init(CYBSP_DEBUG_UART_TX, CYBSP_DEBUG_UART_RX, CY_RETARGET_IO_BAUDRATE);
+    if(result != CY_RSLT_SUCCESS)
+    {
+    	handle_error();
+    }
+
+    /* \x1b[2J\x1b[;H - ANSI ESC sequence for clear screen */
+    printf("\x1b[2J\x1b[;H");
+
+    printf("************************************************************\r\n");
+    printf("PSoC 6 MCU UART Transmit and Receive\r\n");
+    printf("************************************************************\r\n\n");
+    printf(">> Start typing to see the echo on the screen \r\n\n");
+    __enable_irq();
+
+    uint8_t read_data;
+ 
+    for(;;)
+    {
+    	if(CY_SCB_UART_SUCCESS == cyhal_uart_getc(&cy_retarget_io_uart_obj, &read_data, 0))
+    	{
+    		if(CY_SCB_UART_SUCCESS != cyhal_uart_putc(&cy_retarget_io_uart_obj, read_data))
+    		{
+    			handle_error();
+    		}
+    	}
+    	else
+    	{
+    		handle_error();
+    	}
+    }
+}
